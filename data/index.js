71f346dbef706e1630c29192d578c4d38a05446e
StvLi20 <stevenjosueliconamatute@gmail.com>
2025-04-11 20:53:04 -0600

    cambios3

--- data/index.js
+++ data/index.js
@@ -1,24 +1,28 @@
 import { galeria } from "./pages/portafolio";
+import { faqs } from "./pages/faq";
 
 export const getPageContext = (pagePath) => {
     console.log("Page to Load Context:", pagePath);
 
-    
     const commonVariables = {
         galeria: galeria,
+        faqs: faqs
     };
 
     let pageVariables = {};
     console.log("Page been loaded:", pagePath);
     switch (pagePath) {
         case '/index.html':
-            pageVariables = { galeria }; 
+            pageVariables = { galeria };
             break;
         case '/portafolio.html':
-            pageVariables = { galeria }; 
+            pageVariables = { galeria };
+            break;
+        case '/faq.html':
+            pageVariables = { faqs }; // 👈 Añadido para la página de preguntas frecuentes
+            break;
     }
 
-    
     const finalContext = {
         ...commonVariables,
         ...pageVariables
---
+++ data/pages/faq.js
@@ -0,0 +1,19 @@
+export const faqs = [
+    {
+      question: '¿Realizan pedidos personalizados?',
+      answer: '¡Sí! Podemos crear pasteles y postres personalizados para cualquier ocasión. Cuéntanos tu idea y trabajaremos contigo para que sea exactamente como lo imaginas.'
+    },
+    {
+      question: '¿Con cuánto tiempo de anticipación debo hacer un pedido?',
+      answer: 'Recomendamos realizar los pedidos personalizados con al menos 3-5 días de anticipación. Para eventos grandes, como bodas, sugerimos reservar con varias semanas de antelación.'
+    },
+    {
+      question: '¿Tienen opciones sin gluten o para dietas especiales?',
+      answer: 'Sí, ofrecemos opciones sin gluten, veganas y sin azúcar añadido. Por favor, avísanos al momento de hacer tu pedido para adaptar nuestras recetas a tus necesidades.'
+    },
+    {
+      question: '¿Hacen entregas a domicilio?',
+      answer: '¡Claro! Contamos con servicio de entrega a domicilio en [zona/ciudad específica]. Consulta nuestras tarifas y disponibilidad al hacer tu pedido.'
+    }
+  ];
+  
